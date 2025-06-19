83923c676001cbf2d2b212a52f0994e11e9e51cf
poper12 <samyakdhengle89@gmail.com>
2025-06-19 23:23:22 +0530

    config.py

--- config.py
+++ config.py
@@ -4,7 +4,7 @@ env_vars = {
   # Get From my.telegram.org
   "API_ID": "20373005",
   #Get For @BotFather
-  "BOT_TOKEN": "8086700219:AAFANCbRZqMAzNe3vwWLkzKiPhdbW2SCRXE",
+  "BOT_TOKEN": "8086700219:AAFUE3Q8ozqWs9_FPfzgvqdFss21Me_HfVI",
   # Get For tembo.io
   "DATABASE_URL_PRIMARY": "postgresql://neondb_owner:npg_NoDFHanE76qR@ep-twilight-fire-a80mx792-pooler.eastus2.azure.neon.tech/neondb?sslmode=require",
   # Logs Channel Username Without @
